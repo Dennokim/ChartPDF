49d009e3815a3ceb8a428e04cceb3b179f0816ab
Denis <deniskimani2022@gmail.com>
2023-11-10 11:52:55 +0300

    added user sign in and log in

--- chartpdf/src/app/page.tsx
+++ chartpdf/src/app/page.tsx
@@ -1,9 +1,39 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from 'next/link'
 
 export default function Home() {
+  const { userId } = auth();
+  const isAuth = !!userId;
+
   return (
-    <div>
-      <Button>Click Me</Button>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chart with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to charts</Button>}
+          </div>
+
+          <p className=" max-w-xl mt-1 text-lg text-slate-700">
+            Join millions in learning through the power of AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>File upload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>Login to get started</Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
