4a4d941fa5e80c7f6534cd812334b167d809db82
Denis <deniskimani2022@gmail.com>
2023-11-12 15:15:41 +0300

    i have successfully connected to amazon s3 and can upload pdfs to amazon s3

--- chartpdf/src/components/ui/FileUpload.tsx
+++ chartpdf/src/components/ui/FileUpload.tsx
@@ -2,14 +2,28 @@
 import React from "react";
 import { useDropzone } from "react-dropzone";
 import { Inbox, Loader2 } from "lucide-react";
+import { uploadToS3 } from "@/lib/s3";
 
 const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
-    accept: {"application/pdf": ['./pdf']},
-    maxFiles: 1, 
-    onDrop: (acceptedFiles) => {
+    accept: { "application/pdf": ["./pdf"] },
+    maxFiles: 1,
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
-    }
+      const file = acceptedFiles[0];
+
+      if (file.size > 10 * 1024 * 1024) {
+        //file is bigger than 10mb
+        alert("please upload a smaller file");
+        return;
+      }
+      try {
+        const data = await uploadToS3(file);
+        console.log('data', data)
+      } catch (error) {
+        console.log(error);
+      }
+    },
   });
 
   return (
